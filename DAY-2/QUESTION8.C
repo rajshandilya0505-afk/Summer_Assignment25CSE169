bcc53771b80a3d176d7b1f8bec0ba8049ed9f570
= <=>
2026-06-06 21:38:31 +0530

    Save DAY-2/QUESTION8.C to git

---
+++ DAY-2/QUESTION8.C
@@ -0,0 +1,24 @@
+// WRITE A PROGRAM TO CHECK WHETHER A NUMBER IS PALIDROME OR NOT.
+#include <stdio.h>
+
+int main() {
+    int num, original, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome Number");
+    else
+        printf("Not a Palindrome Number");
+
+    return 0;
+}
