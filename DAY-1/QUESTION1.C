0dd8f1648294cb35eb27157e2681fee9a7472799
= <=>
2026-06-08 19:58:16 +0530

    Added Day 1 assignments

---
+++ DAY-1/QUESTION1.C
@@ -0,0 +1,17 @@
+//Write a program to Calculate sum of first N natural numbers.
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+
+    return 0;
+}
---
+++ DAY-1/QUESTION4.C
@@ -0,0 +1,18 @@
+//	Write a program to Count digits in a numbe
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        num = num / 10;
+        count++;
+    }
+
+    printf("Number of digits = %d\n", count);
+
+    return 0;
+}
